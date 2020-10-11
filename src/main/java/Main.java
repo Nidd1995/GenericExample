b6b30be210cf78b072192697eb23abf23bcb03d8
nidhi <tech.nidhiverma@gmail.com>
2020-10-12 00:06:36 +0530

    Implementing generics

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static  void main(String[] args){
+        GenericsExample<String, Integer> obj = new GenericsExample<String, Integer>("Nidhi", 15);
+        obj.print();
+    }
+
+}
